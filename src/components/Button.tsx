4ab31d12c286d80116dc68713db059b18dcc395d
Zack289 <ozack911@gmail.com>
2026-03-17 18:22:44 +0545

    Added the styling in the upper part of the hero section

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import { LinkField } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import clsx from "clsx";
+
+type Props = {
+  buttonLink: LinkField;
+  buttonText: string | null;
+  className?: string;
+};
+
+function Button({ buttonLink, buttonText, className }: Props) {
+  return (
+    <PrismicNextLink
+      field={buttonLink}
+      className={clsx(
+        "teansition-colors rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold tracking-wide text-white uppercase duration-150 hover:bg-orange-700 md:text-2xl",
+        className,
+      )}
+    >
+      {buttonText}
+    </PrismicNextLink>
+  );
+}
+
+export default Button;
