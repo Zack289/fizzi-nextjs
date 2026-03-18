3972cbede236aeb9d1e448f5601af0fb00dae88c
Zack289 <ozack911@gmail.com>
2026-03-18 17:02:01 +0545

    Adding the bubble effect to the hero section

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -13,6 +13,7 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { View } from "@react-three/drei";
 import Scene from "./Scene";
+import { Bubbles } from "./Bubbles";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -100,6 +101,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       className="hero opacity-0"
     >
       <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+        <Bubbles count={300} speed={2} repeat={true}/>
         <Scene />
       </View>
 
