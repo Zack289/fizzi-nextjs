853c74851f889f0d370f67ad010cf100e1cefeee
Zack289 <ozack911@gmail.com>
2026-03-21 14:24:11 +0545

    Addind the gsap magic and making some responsive stuffs for the section

--- src/slices/AlternatingText/index.tsx
+++ src/slices/AlternatingText/index.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { FC } from "react";
+import { Bounded } from "@/components/Bounded";
 import { asText, Content } from "@prismicio/client";
 import {
   PrismicRichText,
   PrismicText,
   SliceComponentProps,
 } from "@prismicio/react";
-import { Bounded } from "@/components/Bounded";
 import { View } from "@react-three/drei";
 import Scene from "./Scene";
+import clsx from "clsx";
 
 /**
  * Props for `AlternatingText`.
@@ -20,7 +20,7 @@ export type AlternatingTextProps =
 /**
  * Component for "AlternatingText" Slices.
  */
-const AlternatingText: FC<AlternatingTextProps> = ({ slice }) => {
+const AlternatingText = ({ slice }: AlternatingTextProps): JSX.Element => {
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -28,22 +28,26 @@ const AlternatingText: FC<AlternatingTextProps> = ({ slice }) => {
       className="alternating-text-container relative bg-yellow-300 text-sky-950"
     >
       <div>
-        <div className="relative grid">
-          <View className="alternating-text-view absolute left-0 top-0 h-screen w-full">
+        <div className="relative z-100 grid">
+          <View className="alternating-text-view absolute top-0 left-0 h-screen w-full">
             <Scene />
           </View>
+
           {slice.primary.text_group.map((item, index) => (
             <div
               key={asText(item.heading)}
               className="alternating-section grid h-screen place-items-center gap-x-12 md:grid-cols-2"
             >
               <div
-                className={index % 2 === 0 ? "col-start-1" : "md:col-start-2"}
+                className={clsx(
+                  index % 2 === 0 ? "col-start-1" : "md:col-start-2",
+
+                  "rounded-lg p-4 backdrop-blur-lg max-md:bg-white/30",
+                )}
               >
                 <h2 className="text-6xl font-bold text-balance">
                   <PrismicText field={item.heading} />
                 </h2>
-
                 <div className="mt-4 text-xl">
                   <PrismicRichText field={item.body} />
                 </div>
@@ -52,7 +56,6 @@ const AlternatingText: FC<AlternatingTextProps> = ({ slice }) => {
           ))}
         </div>
       </div>
-     
     </Bounded>
   );
 };
