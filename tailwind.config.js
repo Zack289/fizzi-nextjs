fb413c16b5157c20a2c6365d9b0da57e4a1b82cc
Zack289 <ozack911@gmail.com>
2026-03-21 14:57:23 +0545

    Improving the animation errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,6 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-alpino)", "sans-serif"],
       },
-      keyframes: {
-        "slide-left": {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-200px)" },
-        },
-      },
-      animation: {
-        "slide-left": "slide-left 3s linear infinite",
-        "spin-slow": "spin 6s linear infinite",
-      },
     },
   },
 };
