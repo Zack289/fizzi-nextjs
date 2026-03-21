7605106b67a119c0af203e2d28618b8fe2596408
Zack289 <ozack911@gmail.com>
2026-03-21 13:38:27 +0545

    Adding the alternating text section in the page

---
+++ src/slices/AlternatingText/Scene.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import FloatingCan from "@/components/FloatingCan";
+import { Environment } from "@react-three/drei";
+
+type Props = {};
+
+const Scene = (props: Props) => {
+  return (
+    <group>
+      <FloatingCan flavor="strawberryLemonade" />
+      <Environment files={"/hdr/lobby.hdr"} environmentIntensity={1.5} />
+    </group>
+  );
+};
+
+export default Scene;
